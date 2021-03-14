9618f74404edb7180e2fff7c2c70b9d431b7e215
YunHaesu <yunhaesu.dev@gmail.com>
2021-03-14 17:17:38 +0900

    React1day

---
+++ src/02/02-9.js
@@ -0,0 +1,14 @@
+// function parse(qs) {
+//   var queryString = qs.substr(1); // queryString = 'banana = 10&apple = 20&orange = 30'
+//   var chunks = qs.split('&');
+//   var result = {};
+//   for (var i = 0; i < chunks.length; i++) {
+//     var parts = chunks[i].split('=');
+//     var key = parts[0]; // key = 'banana'
+//     var value = parts[1]; // value = '10'
+//     result[key] = value; // result = {banana : '10'}
+//   }
+//   return result;
+// }
+
+function parse(qs) {}
